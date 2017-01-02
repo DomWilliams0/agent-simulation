72de4d0dc6b66cb43e21492621768e1d3804f550
Dom Williams <me@domwillia.ms>
2017-01-02 10:25:16 +0000

    Add logging

---
+++ include/util/log.h
@@ -0,0 +1,21 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+
+// TODO put in Makefile
+#define DEBUGGING
+
+#define EXPAND_VA_ARGS(...) __VA_ARGS__
+
+#define LOG_STREAM (stderr)
+
+#define LOG_INFO(msg, ...) fprintf(LOG_STREAM, "%s:%d:%s(): " msg "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__);
+
+#ifdef DEBUGGING
+#define LOG_DEBUG(msg, ...) fprintf(LOG_STREAM, "%s:%d:%s(): " msg "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__);
+#else
+#define LOG_DEBUG(msg, ...) do {} while (0)
+#endif
+
+#endif
