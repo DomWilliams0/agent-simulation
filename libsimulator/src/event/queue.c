b8861198542d61523cad9512560354d5bf51a498
Dom Williams <me@domwillia.ms>
2018-06-27 07:58:29 +0100

    Add debug logging for event queue expansion

--- libsimulator/src/event/queue.c
+++ libsimulator/src/event/queue.c
@@ -1,3 +1,4 @@
+#include <util/log.h>
 #include "queue.h"
 #include "event.h"
 
@@ -21,7 +22,17 @@ void ev_queue_destroy(struct ev_queue *queue)
 void ev_queue_push(struct ev_queue *queue,
                    struct ev_event *evt)
 {
+#ifdef DEBUGGING
+	int cap_old = ev_queue_write(queue)->capacity;
+#endif
+
 	vec_push(ev_queue_write(queue), *evt);
+
+#ifdef DEBUGGING
+	int cap_new = ev_queue_write(queue)->capacity;
+	if (cap_old != cap_new)
+		LOG_DEBUG("event queue %p expanded capacity to %d", queue, cap_new);
+#endif
 }
 
 void ev_queue_clear_n_flip(struct ev_queue *queue)
